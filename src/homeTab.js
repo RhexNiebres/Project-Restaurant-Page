10b8906d6d3d95400f37bd793524086ba5ff8fff
RhexNiebres <rhexniebres@gmail.com>
2024-12-20 14:51:43 -0800

    add homeTab.js function to create a div content and eexport it by defualt to import it to template.html

--- src/homeTab.js
+++ src/homeTab.js
@@ -1,9 +1,9 @@
 export default function loadHome(content) {
     const homeDiv = document.createElement('div');
-    homeDiv.className = 'module';
-    homeDiv.innerHTML = `
+    homeDiv.className = 'home';
+    homeDiv.innerHTML= `
         <h2>About Us</h2>
         <p>We are a company that offers exceptional services to help you succeed. Our mission is to provide you with top-notch solutions tailored to meet your needs.</p>
     `;
-    content.appendChild(aboutDiv);
+    content.appendChild(homeDiv);
 }
