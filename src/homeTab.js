2f1c2858d2a05e9c4fb68b036c94d786933dac07
RhexNiebres <rhexniebres@gmail.com>
2024-12-20 15:31:46 -0800

    add sections to create div element and seperation on content

--- src/homeTab.js
+++ src/homeTab.js
@@ -1,9 +1,31 @@
 export default function loadHome(content) {
     const homeDiv = document.createElement('div');
     homeDiv.className = 'home';
-    homeDiv.innerHTML= `
-        <h2>N</h2>
-        <p>We are a company that offers exceptional services to help you succeed. Our mission is to provide you with top-notch solutions tailored to meet your needs.</p>
-    `;
+
+    const section1 = document.createElement('div');
+    section1.className = 'section';
+    section1.innerHTML =`
+    <h1>ciri's resto</h1>
+    <p>Here to serve you the best filipino food cuisine's</p>`;
+
+    const section2 = document.createElement('div');
+    section2.className = 'section';
+    section2.innerHTML =`
+    <h2>OPEN HOURS</h2>
+    <ul>
+        <li>monday to sunday</li>
+        <li>9AM - 11PM</li>
+    </ul>`;
+    
+    const section3 = document.createElement('div');
+    section3.className = 'section';
+    section3.innerHTML =`
+    <h2>Location</h2>
+    <p>123 Maine drive, ForestVille </p>`;
+    
+    homeDiv.appendChild(section1);
+    homeDiv.appendChild(section2);
+    homeDiv.appendChild(section3);
+
     content.appendChild(homeDiv);
 }
