7fb39a38c5eb6cca142526e0e4692ebdb243daef
RhexNiebres <rhexniebres@gmail.com>
2024-12-20 15:02:21 -0800

    import aboutTab.js loadAbout function that creates elemnt and append it to content

--- src/homeTab.js
+++ src/homeTab.js
@@ -2,7 +2,7 @@ export default function loadHome(content) {
     const homeDiv = document.createElement('div');
     homeDiv.className = 'home';
     homeDiv.innerHTML= `
-        <h2>About Us</h2>
+        <h2>N</h2>
         <p>We are a company that offers exceptional services to help you succeed. Our mission is to provide you with top-notch solutions tailored to meet your needs.</p>
     `;
     content.appendChild(homeDiv);
